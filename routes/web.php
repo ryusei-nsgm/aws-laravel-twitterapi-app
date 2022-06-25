ac28defd6b5cbfc38a2d01aae088b76e1d6e2e2d
EC2 Default User <ec2-user@ip-192-168-10-10.ap-northeast-1.compute.internal>
2022-06-25 12:51:49 +0000

    ルーティング作成

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/bazzreach/search', 'App\Http\Controllers\BazzReachController@search')->name('bazzreach.search');
+Route::post('/bazzreach/bazzsearch', 'App\Http\Controllers\BazzReachController@bazzsearch')->name('bazzreach.bazzsearch');
+Route::post('/bazzreach/{bazzreach}/analysis', 'App\Http\Controllers\BazzReachController@analysis')->name('bazzreach.analysis');
+Route::get('/bazzreach/{bazzreach}/result', 'App\Http\Controllers\BazzReachController@result')->name('bazzreach.result');
+
+Route::resource('/bazzreach', App\Http\Controllers\BazzReachController::class);
+
+URL::forceScheme('https');
