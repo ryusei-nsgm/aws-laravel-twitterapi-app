798f16d273550d2a230408b76bc9fc6bbb4f1cc1
EC2 Default User <ec2-user@ip-192-168-10-10.ap-northeast-1.compute.internal>
2022-06-26 03:54:18 +0000

    コントローラー作成

---
+++ app/Http/Controllers/BazzReachController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBazzReachRequest;
+use App\Http\Requests\UpdateBazzReachRequest;
+use App\Models\BazzReach;
+
+class BazzReachController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreBazzReachRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBazzReachRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BazzReach  $bazzReach
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BazzReach $bazzReach)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BazzReach  $bazzReach
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BazzReach $bazzReach)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateBazzReachRequest  $request
+     * @param  \App\Models\BazzReach  $bazzReach
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateBazzReachRequest $request, BazzReach $bazzReach)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BazzReach  $bazzReach
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BazzReach $bazzReach)
+    {
+        //
+    }
+}
